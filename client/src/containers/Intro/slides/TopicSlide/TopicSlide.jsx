be47cbbbd969b91ecde71cbf7ea5d97ae0aea399
JasonProcka <jasonprocka@gmail.com>
2018-05-16 23:21:56 -0500

    minor changes

--- client/src/containers/Intro/slides/TopicSlide/TopicSlide.jsx
+++ client/src/containers/Intro/slides/TopicSlide/TopicSlide.jsx
@@ -12,16 +12,34 @@ const TopicSlide = props => (
     <div className={leftPanel}>
       <div className={panelGuide}>
         <h2 className={css.topicInfo__header}>
-          Begin by selecting <i>Polynomials and Nonlinear Functions</i> as your topic.
+          Begin by selecting <i>Factoring and Polynomials</i> as your topic.
         </h2>
         <button
-          className={css.StartSlide__button}
+          className={css.topicInfo__continue}
           onClick={() => props.dispatch(nextSlide())}
         >Continue
         </button>
       </div>
     </div>
-    <div className={rightPanel} />
+    <div className={rightPanel}>
+      <div className={css.TopicSlide__topic}>
+        <h3 className={css.TopicSlide__topic_header}>Algebra 1 Topics</h3>
+        <ul className={css.TopicSlide__topic_list}>
+          <li>Expressions, equations and functions</li>
+          <li>Real numbers</li>
+          <li>Solving linear equations</li>
+          <li>Linear functions</li>
+          <li>Linear equations</li>
+          <li>Linear inequalitites</li>
+          <li>Systems of linear equations and inequalities</li>
+          <li>Exponents and exponential functions</li>
+          <li>Factoring and polynomials</li>
+          <li>Quadratic equations</li>
+          <li>Radical expressions</li>
+          <li>Rational expressions</li>
+        </ul>
+      </div>
+    </div>
   </Slide>
 )
 
